d911c4c802dced7b5a766e16d32014b57337d821
postolenko <denis.postolenko@gmail.com>
2020-04-23 19:24:40 +0300

    win

--- js/scripts.js
+++ js/scripts.js
@@ -6,26 +6,61 @@ function getAnimation() {
   });
 }
 
-$(window).load(function() {
+function getWrapperParams() {
+	if(bodyWidth <= 767) {
+		$(".wrapper").css({
+			"padding-top" : $("#header").height() + "px"
+		});
+	} else {
+		$(".wrapper").css({
+			"padding-top" : 0
+		});
+	}
+}
 
-
-
-});
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
 
 $(window).resize(function() {
-
-  getAnimation();
-
+	bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+	getWrapperParams();
+	getAnimation();
 });
 
 $(document).scroll(function() {
-
   getAnimation();
-
 });
 
 $(document).ready(function() {
+	getWrapperParams();
+	getAnimation();
 
-  getAnimation();
+  // ----------
+  $(".respmenubtn").click(function(e) {
+      e.preventDefault();
+      if( $("#resp_nav").is(":hidden") ) {
+          $("#resp_nav").fadeIn(300);
+          $(this).addClass("active");
+      } else {
+          $("#resp_nav").fadeOut(300);
+          $(this).removeClass("active");
+      }
+  });
+  $("#resp_nav .close_btn").on("click", function(e) {
+      e.preventDefault();
+      $("#resp_nav").fadeOut(300);
+      $(".respmenubtn").removeClass("active");
+  });
+  $(this).keydown(function(eventObject){
+      if (eventObject.which == 27 &&
+          $("#resp_nav").is(":visible") &&
+          bodyWidth <= 767) {
+              $("#resp_nav").fadeOut(300);
+              $(".respmenubtn").removeClass("active");
+      }
+  });
 
 });
